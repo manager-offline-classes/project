a4ca6019292d0005d1f886113ffa09ef5c1f80cd
unknown <nam2002bv@gmail.com>
2024-02-28 17:27:58 +0700

    update auth

--- src/http/middlewares/guest.middleware.js
+++ src/http/middlewares/guest.middleware.js
@@ -1,4 +1,5 @@
-module.exports = (req, res, next) => {
+const { LoginToken } = require("../../models/index");
+module.exports = async (req, res, next) => {
   console.log(`guest middleware`);
 
   if (req.user) {
@@ -7,12 +8,24 @@ module.exports = (req, res, next) => {
     if (!req.user) {
       next();
     }
-    if (req.user.typeId === 1) {
-      return res.redirect("/student");
-    } else if (req.user.typeId === 2) {
-      return res.redirect("/teacher");
-    } else if (req.user.typeId === 3) {
-      return res.redirect("/admin");
+    const loginToken = await LoginToken.findOne({
+      where: {
+        userId: req.user.id,
+      },
+    });
+    if (
+      loginToken?.token === req.cookies.loginToken &&
+      req.cookies.loginToken
+    ) {
+      console.log(324234);
+      if (req.user.typeId === 1) {
+        return res.redirect("/student");
+      } else if (req.user.typeId === 2) {
+        return res.redirect("/teacher");
+      } else if (req.user.typeId === 3) {
+        console.log(24234);
+        return res.redirect("/admin");
+      }
     }
   }
   next();
